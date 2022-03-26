b2179ec9577f2bb9c9a4a8532101f2bf111f9a8b
VladislavGuliy <vladgulij777@gmail.com>
2022-03-26 15:44:25 +0200

    Site routing, page navigation

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 import { Navbar,Nav, FormControl, Container, Form, Button } from 'react-bootstrap';
 import logo from './logo192.png';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+
+import Home from '../Pages/Home';
+import About from '../Pages/About';
+import Contacts from '../Pages/Contacts';
+import Blog from '../Pages/Blog';
+
 
 export default class Header extends Component {
   render() {
@@ -42,6 +49,17 @@ export default class Header extends Component {
 
             </Container>
         </Navbar>
+        <Router>
+            <Routes>
+
+              <Route exact path="/" element={<Home/>}/>
+              <Route exact path="/about" element={<About/>}/>
+              <Route exact path="/contacts" element={<Contacts/>}/>
+              <Route exact path="/blog" element={<Blog/>}/>
+            
+
+            </Routes>
+        </Router>
     </>
     
     )
---
+++ src/Pages/Blog.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Blog extends Component {
+  render() {
+    console.log(5);
+    return (
+      <div>
+        {console.log(1)}
+        <h1>Blog</h1>
+      </div>
+    )
+  }
+} 
