924963ad7716e99eedb670401b5888cb955f60ca
VladislavGuliy <vladgulij777@gmail.com>
2022-03-28 18:38:36 +0300

    Creating a Contacs Page Using Contact Forms

--- src/Pages/Contacts.js
+++ src/Pages/Contacts.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react';
+import { Container, Form, Button } from 'react-bootstrap';
 
 export default class Contacts extends Component {
   render() {
+    const divStyle = {
+      marginTop: "90px",
+      width: '500px'
+    }
     return (
-      <div>
-        <h1>Contacts</h1>
-      </div>
+      <Container style={divStyle}>
+        <h1 className='text-center'>Contact us</h1>
+        <Form>
+          <Form.Group controlId="formBasicEmail">
+             <Form.Label >Email adress</Form.Label>
+             <Form.Control type="email" placeholder="Enter email"/>
+               <Form.Text>
+                   We'll never share your email with anyone else
+                </Form.Text>
+
+          </Form.Group> 
+
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Exmple textarea</Form.Label>
+              <Form.Control as="textarea" rows="3"/>
+          </Form.Group>
+
+          <Form.Group controlId="formBasicCheckBox">
+              <Form.Check type="checkbox" label="Check me out"/>
+          </Form.Group>
+
+          <Button variant="primary" type="submit">Submit</Button>
+
+        </Form>
+      </Container>
     )
   }
 }
